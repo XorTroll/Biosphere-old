722a5038c9d580edd8b42711e4dbeb4364c82eda
XorTroll <xabofdez@outlook.es>
2019-01-27 00:04:39 +0100

    New test, several changes

--- BiosphereTest/Source/Main.cpp
+++ BiosphereTest/Source/Main.cpp
@@ -1,30 +1,38 @@
 #include <bio/Biosphere>
 #include <string>
 
-// Latest test:
-// - Press A to ask for a user, and then error with error code 2101-101 and the user's name.
-// - Press Plus to exit.
+// Latest test: as a system applet, attempts to launch Goldleaf as an installed title. MUST BE RUN AS A QLAUNCH REPLACEMENT ON ATMOSPHERE!!
 
 int main()
 {
     bio::os::OverrideHeap(0x10000000).AssertOk();
-    bio::app::Initialize(bio::app::RunMode::LibraryApplet);
-
-    bio::input::InputManager *inm = new bio::input::InputManager(0);
-    bio::input::Player *handheld = inm->GetPlayer(bio::input::Controller::HandHeld);
-
-    while(true)
-    {
-        u64 ipt = handheld->GetInput();
-        if(ipt & bio::input::Key::A)
-        {
-            bio::app::WebApplet *web = new bio::app::WebApplet("https://github.com/XorTroll");
-            web->Show().AssertOk();
-            delete web;
-        }
-        else if(ipt & bio::input::Key::Plus) break;
-    }
-
-    bio::app::Finalize();
+    bio::sm::Initialize();
+    // appletAE
+    bio::applet::ae::AeService *ae = bio::applet::ae::Initialize().AssertOk();
+    bio::err::SetDefaultThrowMode(bio::err::ThrowMode::Fatal);
+    bio::svc::SleepThread(1000000000);
+    // appletAE::OpenSystemAppletProxy(u64 Reserved = 0) -> ISystemAppletProxy
+    bio::applet::ae::SystemAppletProxy *isap = ae->OpenSystemAppletProxy(0).AssertOk();
+    u32 oihmf = 0;
+    isap->ProcessRequest<20>(bio::hipc::OutObjectId<0>(oihmf)).AssertOk();
+    // ISystemAppletProxy::GetHomeMenuFunctions() -> IHomeMenuFunctions
+    bio::hipc::Object *ihmf = new bio::hipc::Object(isap, oihmf);
+    // IHomeMenuFunctions::UnlockForeground()
+    ihmf->ProcessRequest<12>(bio::hipc::Simple()).AssertOk();
+    u32 oiac = 0;
+    // ISystemAppletProxy::GetApplicationCreator() -> IAppicationCreator
+    isap->ProcessRequest<22>(bio::hipc::OutObjectId<0>(oiac)).AssertOk();
+    bio::hipc::Object *iac = new bio::hipc::Object(isap, oiac);
+    u32 oiaa = 0;
+    // IApplicationCreator::CreateApplication(u64 ApplicationId) -> IApplicationAccessor
+    iac->ProcessRequest<0>(bio::hipc::InRaw<u64>(0x050032a5cf12e000), bio::hipc::OutObjectId<0>(oiaa)).AssertOk();
+    bio::hipc::Object *iaa = new bio::hipc::Object(iac, oiaa);
+    // IApplicationAccessor::Start()
+    iaa->ProcessRequest<10>(bio::hipc::Simple()).AssertOk();
+    bio::svc::SleepThread(1000000000);
+    // IApplicationAccessor::RequestForApplicationToGetForeground()
+    iaa->ProcessRequest<101>(bio::hipc::Simple()).AssertOk();
+    while(true);
+    bio::sm::Finalize();
     return 0;
 }
